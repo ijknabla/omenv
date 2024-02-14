98e03cbb08da67e10449a2e5ec0396e3b23446ec
ijknabla <ijknabla@gmail.com>
2024-02-14 12:38:42 +0900

    Fix code format

--- ombuild.py
+++ ombuild.py
@@ -1,10 +1,11 @@
 import argparse
-from pathlib import Path
-from asyncio import run, create_subprocess_exec
 import os
 import shutil
+from asyncio import create_subprocess_exec, run
+from pathlib import Path
 
 HERE = Path(__file__).parents[0].resolve()
+
 
 async def main() -> None:
     parser = argparse.ArgumentParser()
@@ -12,18 +13,22 @@ async def main() -> None:
     args = parser.parse_args()
     tag: str = args.tag
 
-    src = HERE/"src"/tag
-    build = HERE/"build"/tag
-    install = HERE/tag
+    src = HERE / "src" / tag
+    build = HERE / "build" / tag
+    install = HERE / tag
 
     os.makedirs(src.parents[0], exist_ok=True)
     git_clone = await create_subprocess_exec(
-        "git", "clone", "--recursive", "-b", tag,
+        "git",
+        "clone",
+        "--recursive",
+        "-b",
+        tag,
         "https://github.com/OpenModelica/OpenModelica.git",
         f"{src}",
     )
     await git_clone.wait()
-    
+
     for CMakeList in src.rglob("CMakeLists.txt"):
         CMakeList.write_text(
             CMakeList.read_text().replace(
@@ -32,12 +37,14 @@ async def main() -> None:
             )
         )
 
-
-
     os.makedirs(build, exist_ok=True)
 
     cmake_cmd = [
-        "cmake", "-DCMAKE_BUILD_TYPE=Release", f"-DCMAKE_INSTALL_PREFIX={install}", f"-S={src}", f"-B={build}"
+        "cmake",
+        "-DCMAKE_BUILD_TYPE=Release",
+        f"-DCMAKE_INSTALL_PREFIX={install}",
+        f"-S={src}",
+        f"-B={build}",
     ]
 
     cmake_process = await create_subprocess_exec(*cmake_cmd)
@@ -45,9 +52,7 @@ async def main() -> None:
 
     make_process = await create_subprocess_exec("make", f"-C{build}", "-j4", "install")
     await make_process.wait()
-    
-    
-    
+
+
 if __name__ == "__main__":
     run(main())
-                            
